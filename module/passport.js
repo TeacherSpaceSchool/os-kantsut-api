646a7b61d087dd7d53d4d9a02011e638419a5bf2
TeacherSpaceSchool <azyk.store@gmail.com>
2021-10-20 16:25:28 +0600

    check

--- module/passport.js
+++ module/passport.js
@@ -31,14 +31,11 @@ let start = () => {
     jwtOptions.secretOrKey=jwtsecret;
     passport.use(
         new JwtStrategy(jwtOptions, function (payload, done) {
-            console.log('JwtStrategy1', JSON.stringify(payload))
             User.findOne({login:payload.login}, (err, user) => {
-                console.log('JwtStrategy2', JSON.stringify(err), JSON.stringify(user))
                 if (err) {
                     return done(err)
                 }
                 if (user&&user.status==='active') {
-                    console.log('JwtStrategy3', JSON.stringify(user))
                     return done(null, user)
                 } else {
                     return done(null, false)
@@ -99,8 +96,9 @@ const getuser = async (req, res, func) => {
 
 const verifydeuserGQL = async (req, res) => {
     return new Promise((resolve) => { passport.authenticate('jwt', async function (err, user) {
+        console.log('verifydeuserGQL0', !!req.cookies )
         try{
-            console.log('verifydeuserGQL1', !!req.cookies, JSON.stringify(req.cookies.pinCode), JSON.stringify(user), JSON.stringify(user.pinCode), )
+            console.log('verifydeuserGQL1', JSON.stringify(req.cookies.pinCode), JSON.stringify(user), JSON.stringify(user.pinCode), )
             user.checkedPinCode = req.cookies&&req.cookies.pinCode===user.pinCode
             resolve(user)
         } catch (err) {
@@ -199,6 +197,12 @@ const signinuserGQL = (req, res) => {
                     const token = await jwt.sign(payload, jwtsecret); //здесь создается JWT
                     await res.clearCookie('jwt');
                     await res.cookie('jwt', token, {maxAge: 500*24*60*60*1000 });
+                    console.log('signinuserGQL', {
+                        role: user.role,
+                        status: user.status,
+                        login: user.login,
+                        _id: user._id
+                    } )
                     resolve({
                         role: user.role,
                         status: user.status,
